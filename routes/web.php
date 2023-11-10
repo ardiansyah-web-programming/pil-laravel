d85bd6852aed93520c4f271a3a20c26b225239c9
unknown <ardhytupank21@gmail.com>
2023-11-10 08:23:17 +0700

    Menggunakan bootstrap 5

--- routes/web.php
+++ routes/web.php
@@ -30,28 +30,3 @@ Route::get('profile', function () {
     // return view('profile', compact('nama'));
     return view('profile')->with('nama', $nama);
 });
-
-Route::get('tabel', function () {
-    return view('pertemuan5/tabel');
-});
-
-Route::get('tabel_e1', function () {
-    return view('pertemuan5_e1/tabel');
-});
-
-Route::get('tabel_d1', function () {
-    return view('d1/pertemuan5/tabel');
-});
-
-Route::get('tabel_a1', function () {
-    $kode_pelanggan = ['PEL001', 'PEL003', 'PEL005'];
-    $nama = ['Agus', 'Joko', 'Mahmud'];
-    $alamat = ['Medan', 'Tembung', 'Johor'];
-    $jumlah = count($alamat);
-    return view('a1/pertemuan5/tabel')->with([
-        'x' => $kode_pelanggan,
-        'y' => $nama,
-        'z' => $alamat,
-        'count' => $jumlah
-    ]);
-});
