c113e30fd8835792e006173d10b85211a79d6ef9
unknown <ardhytupank21@gmail.com>
2023-11-10 09:20:23 +0700

    Membuat array yang kemudian dipanggil kedalam tabel mahasiswa menggunakan while

--- routes/web.php
+++ routes/web.php
@@ -22,17 +22,14 @@ Route::get('about', function () {
 });
 
 Route::get('mahasiswa', function () {
-    return view('mahasiswa');
+    $npm = [123,124,125,126];
+    $nama = ['Jono','Joni','Juna','Jena'];
+    $jumlah = count($npm);
+    return view('mahasiswa',compact('npm','jumlah','nama'));
 });
 
 Route::get('profile', function () {
     $nama = 'Fulan';
     // return view('profile', compact('nama'));
     return view('profile')->with('nama', $nama);
-});
-
-Route::get('array',function(){
-    for ($i=1; $i <= 5; $i++) { 
-        echo 'Hello World ' . $i . 'x<br>';
-    }
 });
